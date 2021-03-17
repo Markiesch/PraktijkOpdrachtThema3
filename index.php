5a8c2b2a469c4e3e0761560a2c41eeb8c10d13a4
Markiesch <marksmeel@gmail.com>
2021-03-17 15:25:45 +0100

    Backup Commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title></title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="images/logo_1.png" alt="Tandartspraktijk Den Bosch">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Behandelingen</a></li>
+                <li><a href="">Inschrijven</a></li>
+                <li><a href="">Afspraken</a></li>
+                <li><a href="">Over Ons</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Blog</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+
+    <footer class="footer">
+        <div class="footer__container">
+            <div class="footer--intro">
+                <p>WE ZIEN U GRAAG SNEL IN ONZE PRAKTIJK!</p>
+            </div>
+            <div class="footer__body--container">
+                <div class="footer__links--container">
+                    <div>
+                        <h3>Tandarts Den Bosch</h3>
+                        <li>Over ons</li>
+                        <li>Inschrijven</li>
+                        <li>Afspraak maken</li>
+                        <li>Vacatures</li>
+                        <li>Algemene voorwaarden</li>
+                        <li>Privacy beleid</li>
+                        <li>Contact</li>
+                    </div>
+                    <div>
+                        <h3>Tandarts Den Bosch</h3>
+                        <li>Over ons</li>
+                        <li>Inschrijven</li>
+                        <li>Afspraak maken</li>
+                        <li>Vacatures</li>
+                        <li>Algemene voorwaarden</li>
+                        <li>Privacy beleid</li>
+                        <li>Contact</li>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+            <p>© Tandarts Den Bosch 2020</p>
+            <p>​De beste tandarts van Den Bosch e.o. met hoge kwaliteit voor een betaalbaar tarief!</p>
+        </div>
+    </footer>
+</body>
+</html>
