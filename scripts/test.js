baef03212c2b93b37b0bf1f46a83203526c98972
Markiesch <marksmeel@gmail.com>
2021-03-18 14:14:40 +0100

    Added feuture that will will change the color based on their answer

--- scripts/test.js
+++ scripts/test.js
@@ -4,6 +4,12 @@
     File:       scripts/test.js
 */
 
+const colors = {
+    green: "#0dd66c",
+    orange: "#fb6340",
+    red: "#f5365c",
+};
+
 const submitBtn = document.querySelector(".submit");
 const resultElement = document.querySelector(".result");
 const selectFields = [...document.querySelectorAll("select")];
@@ -12,9 +18,31 @@ submitBtn.addEventListener("click", () => {
     resultElement.innerHTML = "Uitslag bereken...";
     let result = 0;
     for (const selectField of selectFields) {
-        let { value } = selectField;
-        value = parseInt(value);
-        result = result + value;
+        let { value, selectedIndex, options } = selectField;
+        const option = [...options];
+        // console.log(selectedIndex);
+        console.log(option);
+
+        let color;
+        if (option.length == 3) {
+            if (!selectedIndex) {
+                color = colors.green;
+            } else if (selectedIndex == 1) {
+                color = colors.orange;
+            } else {
+                color = colors.red;
+            }
+        } else {
+            if (!selectedIndex) {
+                color = colors.green;
+            } else {
+                color = colors.red;
+            }
+        }
+        const parent = selectField.parentElement;
+        selectField.style.backgroundColor = color;
+
+        result += parseInt(value);
     }
     resultElement.innerHTML = result;
 });
